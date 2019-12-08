b1fdfc05f8cb6ae2173f9eecd56ad3731a5aeba8
Md Sifat <cse.sifat@gmail.com>
2019-12-08 16:02:49 +0600

    design and connection

--- Snake and Ladder/ViewController.swift
+++ Snake and Ladder/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Img1: UIImageView!
+    
+    @IBOutlet weak var Img2: UIImageView!
+    @IBOutlet weak var Img3: UIImageView!
+    @IBOutlet weak var Img4: UIImageView!
+    @IBOutlet weak var Img5: UIImageView!
+    @IBOutlet weak var Img6: UIImageView!
+    @IBOutlet weak var Img7: UIImageView!
+    @IBOutlet weak var Img8: UIImageView!
+    @IBOutlet weak var Img9: UIImageView!
+    @IBOutlet weak var Img10: UIImageView!
+    @IBOutlet weak var Img11: UIImageView!
+    @IBOutlet weak var Img12: UIImageView!
+    @IBOutlet weak var Img13: UIImageView!
+    @IBOutlet weak var Img14: UIImageView!
+    @IBOutlet weak var Img15: UIImageView!
+    @IBOutlet weak var Img16: UIImageView!
+    @IBOutlet weak var Img17: UIImageView!
+    @IBOutlet weak var Img18: UIImageView!
+    @IBOutlet weak var Img19: UIImageView!
+    @IBOutlet weak var Img20: UIImageView!
+    @IBOutlet weak var Img21: UIImageView!
+    @IBOutlet weak var Img22: UIImageView!
+    @IBOutlet weak var Img23: UIImageView!
+    @IBOutlet weak var Img24: UIImageView!
+    @IBOutlet weak var Img25: UIImageView!
+    @IBOutlet weak var Img26: UIImageView!
+    @IBOutlet weak var Img27: UIImageView!
+    @IBOutlet weak var Img28: UIImageView!
+    @IBOutlet weak var Img29: UIImageView!
+    @IBOutlet weak var Img30: UIImageView!
+    @IBOutlet weak var Img31: UIImageView!
+    @IBOutlet weak var Img32: UIImageView!
+    @IBOutlet weak var Img33: UIImageView!
+    @IBOutlet weak var Img34: UIImageView!
+    @IBOutlet weak var Img35: UIImageView!
+    @IBOutlet weak var Img36: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
